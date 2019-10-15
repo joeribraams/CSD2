e5620c4ec1f19bec4b7291e81ba82a3921a13f07
joeri braams <joeribraams@gmail.com>
2019-10-15 12:46:41 +0200

    Added comments and did some cleaning

--- python_basics/IrregularBeatGenerator.py
+++ python_basics/IrregularBeatGenerator.py
@@ -1,10 +1,9 @@
-# Import libraries and assign alias
-import random as ra
+# Import libraries and assign aliases
+import random
 import simpleaudio as sa
-import time as t
-import sched as sch
-import threading as th
-import math as m
+import time
+import threading
+import math
 from midiutil import MIDIFile
 from datetime import datetime
 
@@ -17,6 +16,7 @@ kick = sa.WaveObject.from_wave_file("kick.wav")
 while True:
     measure = list(input("Enter your time signature: ").split('/'))
     if len(measure) == 2 and measure[0].isdigit() and measure[1].isdigit() and int(measure[0]) >= 1 and int(measure[1]) >= 1:
+        # Calculates amount of 16th notes for given time signature
         sixteenths = (int(measure[0])*4)/(int(measure[1])/4)
         if sixteenths > 20 or sixteenths < 1:
             print('Unsupported time signature.')
@@ -54,11 +54,12 @@ while True:
         else:
             quit()
     else:
+        # Calculates time for 16th note step from bpm
         note_time = (60000/float(bpm))/4000
         break
 
 # Probability table for the 3 instruments, every second order list is a 16th
-# the 3 floats are the probabilities for hihat, snare and kick respectively
+# The 3 floats are the probabilities for hihat, snare and kick respectively
 prob = [[1, 0, 1], [0.125, 0, 0], [0.75, 0.25, 0.25], [0.25, 0.125, 0],
     [1, 0.75, 0.25], [0.125, 0, 0], [0.875, 0.125, 0.5], [0.125, 0.125, 0.125],
     [1, 0.125, 0.5], [0.25, 0, 0.125], [0.875, 0.25, 0.25],
@@ -72,48 +73,50 @@ sample = [hihat, snare, kick]
 # Makes empty list for the results of the comparison
 play_notes = [[0, 0, 0] for i in range(int(sixteenths))]
 
-# Compares te probabilities to a random float, sets place in list to true if higher
+# Compares the probabilities to a random float, sets place in list to true if higher
 for i in range(int(sixteenths)):
     for x in range(3):
-        if ra.random() < prob[i][x]:
+        if random.random() < prob[i][x]:
             play_notes[i][x] = 1
 
-# boolean to stop the playback loop
+# Boolean to stop the playback loop
 stop_playback = 0
 
-# playback function
+# Playback function
 def playback():
     while True:
-        start_time = t.time()
+        start_time = time.time()
+        # Loops through the list of 16th note steps and tests all 3 instruments
         for i in range(int(sixteenths)):
             for x in range(3):
                 if play_notes[i][x] == 1:
                     sample[x].play()
-            while t.time() - start_time < int(i+1) * note_time:
-                t.sleep(0.001)
+            while time.time() - start_time < int(i+1) * note_time:
+                time.sleep(0.001)
         if stop_playback:
             break
 
-# Make the thread
-playbackthread = th.thread(target=playback)
+# Makes and starts the thread
+playbackthread = threading.thread(target=playback)
 playbackthread.daemon = True
-
-# Start the thread
 playbackthread.start()
 
 # Stops thread with any input
 input("Press enter to stop")
 stop_playback = 1
 
-# Wait for both threads to finish
+# Wait for thread to finish
 playbackthread.join()
 
-MyMIDI = MIDIFile(1) # One track, defaults to format 1 (tempo track automatically created)
+# Creates the midi file with tempo and time signature
+MyMIDI = MIDIFile(1)
 MyMIDI.addTempo(0, 0, int(bpm))
-MyMIDI.addTimeSignature(0, 0, int(measure[0]), int(m.log2(int(measure[1]))), 24, 8)
+MyMIDI.addTimeSignature(0, 0, int(measure[0]), int(math.log2(int(measure[1]))), 24, 8)
 
+# Writes notes to midi file
 for i in range(int(sixteenths)):
     for x in range(3):
+        # Choose note per drum according to general midi
         if x == 0:
             pitch = 42
         elif x == 1:
@@ -121,13 +124,13 @@ for i in range(int(sixteenths)):
         elif x == 2:
             pitch = 36
         if play_notes[i][x] == 1:
+            # Probability gets scaled to velocity, higher chance is more likely to be an accent
             MyMIDI.addNote(0, 9, pitch, (i/4), 0.25, int(60 + (67 * prob[i][x])))
 
-
+# Asks to save file, will save with time signature and current time in filename
 save_file = input("Do you want to save this beat? Y/N ").upper()
 if save_file == "Y":
     filename = measure[0] + "_" + measure[1] + "_beat_at_" + datetime.now().strftime("%H:%M:%S") + ".mid"
-    print(filename)
     with open(filename, "wb") as output_file:
         MyMIDI.writeFile(output_file)
 else:
