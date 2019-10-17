8eb724f1b891df50b573819107fee55f76c709d3
joeri braams <joeribraams@gmail.com>
2019-10-17 17:47:11 +0200

    Added relay function and more comments

--- python_basics/IrregularBeatGenerator.py
+++ python_basics/IrregularBeatGenerator.py
@@ -18,7 +18,7 @@ while True:
     if len(measure) == 2 and measure[0].isdigit() and measure[1].isdigit() and int(measure[0]) >= 1 and int(measure[1]) >= 1:
         # Calculates amount of 16th notes for given time signature
         sixteenths = (int(measure[0])*4)/(int(measure[1])/4)
-        if sixteenths > 20 or sixteenths < 1:
+        if sixteenths > 28 or sixteenths < 1:
             print('Unsupported time signature.')
             retry_tms = input("Retry? Y/N ").upper()
             if retry_tms == "Y":
@@ -28,7 +28,7 @@ while True:
         else:
             break
     else:
-        print('Invalid input, please enter two positive numbers, seperated by a "/".')
+        print('Invalid input, please enter two positive, whole numbers, seperated by a "/".')
         retry_tms = input("Retry? Y/N ").upper()
         if retry_tms == "Y":
             continue
@@ -40,14 +40,14 @@ while True:
 while True:
     bpm = input("Enter your desired BPM: ")
     if not bpm.isdigit():
-        print('Invalid input, please enter a positive number.')
+        print('Invalid input, please enter a positive, whole number.')
         retry_tmp = input("Retry? Y/N ").upper()
         if retry_tmp == "Y":
             continue
         else:
             quit()
     if float(bpm) > 1000 or float(bpm) < 1:
-        print('Unsupported tempo.')
+        print('Unsupported tempo, please stay within 1 and 999 BPM.')
         retry_tmp = input("Retry? Y/N ").upper()
         if retry_tmp == "Y":
             continue
@@ -65,73 +65,81 @@ prob = [[1, 0, 1], [0.125, 0, 0], [0.75, 0.25, 0.25], [0.25, 0.125, 0],
     [1, 0.125, 0.5], [0.25, 0, 0.125], [0.875, 0.25, 0.25],
     [0.125, 0.125, 0.25], [1, 0.75, 0.25], [0.25, 0, 0], [0.75, 0.25, 0.25],
     [0.25, 0.125, 0], [1, 0.125, 0.5], [0.25, 0, 0.125], [0.875, 0.25, 0.25],
-    [0.125, 0.125, 0.25]]
+    [0.125, 0.125, 0.25], [1, 0.75, 0.25], [0.125, 0, 0], [0.875, 0.125, 0.5],
+    [0.125, 0.125, 0.125], [1, 0.125, 0.5], [0.25, 0.125, 0],
+    [0.875, 0.25, 0.25], [0.125, 0.25, 0.125]]
 
 # Makes a list of the 3 samples for playback
 sample = [hihat, snare, kick]
 
-# Makes empty list for the results of the comparison
-play_notes = [[0, 0, 0] for i in range(int(sixteenths))]
+# Loop for replaying with the same settings
+while True:
+    # Compares the probabilities to a random float, sets place in empty list to true if higher
+    play_notes = [[0, 0, 0] for i in range(int(sixteenths))]
+    for i in range(int(sixteenths)):
+        for x in range(3):
+            if random.random() < prob[i][x]:
+                play_notes[i][x] = 1
 
-# Compares the probabilities to a random float, sets place in list to true if higher
-for i in range(int(sixteenths)):
-    for x in range(3):
-        if random.random() < prob[i][x]:
-            play_notes[i][x] = 1
+    # Boolean to stop the playback loop
+    stop_playback = 0
 
-# Boolean to stop the playback loop
-stop_playback = 0
+    # Playback function
+    def playback():
+        while True:
+            start_time = time.time()
+            # Loops through the list of 16th note steps and tests all 3 instruments
+            for i in range(int(sixteenths)):
+                for x in range(3):
+                    if play_notes[i][x] == 1:
+                        sample[x].play()
+                while time.time() - start_time < int(i+1) * note_time:
+                    time.sleep(0.001)
+            if stop_playback:
+                break
 
-# Playback function
-def playback():
-    while True:
-        start_time = time.time()
-        # Loops through the list of 16th note steps and tests all 3 instruments
-        for i in range(int(sixteenths)):
-            for x in range(3):
-                if play_notes[i][x] == 1:
-                    sample[x].play()
-            while time.time() - start_time < int(i+1) * note_time:
-                time.sleep(0.001)
-        if stop_playback:
-            break
+    # Makes and starts the thread
+    playbackthread = threading.Thread(target=playback)
+    playbackthread.daemon = True
+    playbackthread.start()
 
-# Makes and starts the thread
-playbackthread = threading.thread(target=playback)
-playbackthread.daemon = True
-playbackthread.start()
+    # Stops thread with any input
+    input("Press enter to stop")
+    stop_playback = 1
 
-# Stops thread with any input
-input("Press enter to stop")
-stop_playback = 1
+    # Wait for thread to finish
+    playbackthread.join()
 
-# Wait for thread to finish
-playbackthread.join()
+    # Creates the midi file with tempo and time signature
+    MyMIDI = MIDIFile(1)
+    MyMIDI.addTempo(0, 0, int(bpm))
+    MyMIDI.addTimeSignature(0, 0, int(measure[0]), int(math.log2(int(measure[1]))), 24, 8)
 
-# Creates the midi file with tempo and time signature
-MyMIDI = MIDIFile(1)
-MyMIDI.addTempo(0, 0, int(bpm))
-MyMIDI.addTimeSignature(0, 0, int(measure[0]), int(math.log2(int(measure[1]))), 24, 8)
+    # Writes notes to midi file
+    for i in range(int(sixteenths)):
+        for x in range(3):
+            # Choose note per drum according to general midi
+            if x == 0:
+                pitch = 42
+            elif x == 1:
+                pitch = 38
+            elif x == 2:
+                pitch = 36
+            if play_notes[i][x] == 1:
+                # Probability gets scaled to velocity, higher chance is more likely to be an accent
+                MyMIDI.addNote(0, 9, pitch, (i/4), 0.25, int(60 + (67 * prob[i][x])))
 
-# Writes notes to midi file
-for i in range(int(sixteenths)):
-    for x in range(3):
-        # Choose note per drum according to general midi
-        if x == 0:
-            pitch = 42
-        elif x == 1:
-            pitch = 38
-        elif x == 2:
-            pitch = 36
-        if play_notes[i][x] == 1:
-            # Probability gets scaled to velocity, higher chance is more likely to be an accent
-            MyMIDI.addNote(0, 9, pitch, (i/4), 0.25, int(60 + (67 * prob[i][x])))
+    # Asks to save file, will save with time signature and current time in filename
+    save_file = input("Do you want to save this beat? Y/N ").upper()
+    if save_file == "Y":
+        filename = measure[0] + "_" + measure[1] + "_beat_at_" + datetime.now().strftime("%H:%M:%S") + ".mid"
+        with open(filename, "wb") as output_file:
+            MyMIDI.writeFile(output_file)
+            print("Saved as: " + filename)
 
-# Asks to save file, will save with time signature and current time in filename
-save_file = input("Do you want to save this beat? Y/N ").upper()
-if save_file == "Y":
-    filename = measure[0] + "_" + measure[1] + "_beat_at_" + datetime.now().strftime("%H:%M:%S") + ".mid"
-    with open(filename, "wb") as output_file:
-        MyMIDI.writeFile(output_file)
-else:
-    quit()
+    # Asks to make a new beat with the same settings
+    replay = input("Retry with the same settings? Y/N ").upper()
+    if replay == "Y":
+        continue
+    else:
+        quit()
