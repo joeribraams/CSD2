76535ad528e8cb6016667ee5caaf3bc323b62aec
joeri braams <joeribraams@gmail.com>
2019-12-16 16:39:52 +0100

    Added pitchcounter

--- Synthsong/main.cpp
+++ Synthsong/main.cpp
@@ -11,6 +11,7 @@
 
 float pitch1 = 523.2;
 float pitch2 = 130.8;
+int pitchcount = 0;
 
 /*
  * NOTE: jack2 needs to be installed
@@ -33,41 +34,37 @@ int main(int argc,char **argv)
   subSynth subsynth;
 
   // inits the note generators
-  generator generator1;
-  generator generator2;
+  generator generator;
 
   //sets start time for note generation
-  auto start_time1 = std::chrono::high_resolution_clock::now();
-  auto start_time2 = std::chrono::high_resolution_clock::now();
+  auto start_time = std::chrono::high_resolution_clock::now();
 
   //assign a function to the JackModule::onProces
-  jack.onProcess = [&addsynth, &subsynth, &generator1, &generator2, &start_time1, &start_time2]
+  jack.onProcess = [&addsynth, &subsynth, &generator, &start_time]
     (jack_default_audio_sample_t *inBuf, jack_default_audio_sample_t *outBuf, jack_nframes_t nframes) {
 
     for(unsigned int i = 0; i < nframes; i++) {
       // mixes synth outputs and attenuates slightly to reduce chance of clipping
-      outBuf[i] = (addsynth.addSynthOut(pitch1) + subsynth.subSynthOut(pitch2)) * 0.7;
+      outBuf[i] = (addsynth.addSynthOut(pitch1) + subsynth.subSynthOut(pitch2)) * 0.5;
     }
 
     // calculates elapsed milliseconds since last note
-    int time1 = std::chrono::duration_cast<std::chrono::milliseconds>
-      (std::chrono::high_resolution_clock::now() - start_time1).count();
+    int time = std::chrono::duration_cast<std::chrono::milliseconds>
+      (std::chrono::high_resolution_clock::now() - start_time).count();
     // gives a new note for pitch1 every second
-    if (time1 >= 1000)
+    if (time >= 1000)
     {
-      start_time1 = std::chrono::high_resolution_clock::now();
-      pitch1 = generator1.newnote();
+      start_time = std::chrono::high_resolution_clock::now();
+      pitch1 = generator.newnote();
       addsynth.envReset();
-    }
 
-    // calculates elapsed milliseconds since last note
-    int time2 = std::chrono::duration_cast<std::chrono::milliseconds>
-      (std::chrono::high_resolution_clock::now() - start_time2).count();
-    // gives a new note for pitch2 every 2 seconds
-    if (time2 >= 2000)
-    {
-      start_time2 = std::chrono::high_resolution_clock::now();
-      pitch2 = generator2.newnote();
+      if (pitchcount == 1) {
+        pitchcount = 0;
+        pitch2 = generator.newnote();
+      }
+      else {
+        pitchcount = 1;
+      }
     }
 
     return 0;
